b0ffaa91e46b367809593a08c04aaaf1ae57b405
corygross <corygross@0a18d55e-ba55-0410-9722-499b6f56dc25>
2008-10-06 04:30:22 +0000

    more GUI additions

--- trunk/menu.php
+++ trunk/menu.php
@@ -8,15 +8,25 @@
    <tr><td class="menu" onclick="downloadMenuClick();">Download</td></tr>
    </table>
 </td>
+
+<td class="menu_item" onmouseover="showmenu('edit_menu')" onmouseout="hidemenu('edit_menu')">Edit&nbsp;<img src="images/arrow.gif"/><br />
+   <table class="menu" id="edit_menu" width="160px">
+   <tr><td class="menu" onclick="findMenuClick();">Find</td></tr>
+   <tr><td class="menu" onclick="replaceMenuClick();">Replace</td></tr>
+   <tr><td class="menu" onclick="selectAllMenuClick();">Select All</td></tr>
+   </table>
+</td>
+
 <td class="menu_item" onmouseover="showmenu('view_menu')" onmouseout="hidemenu('view_menu')">View&nbsp;<img src="images/arrow.gif"/><br />
    <table class="menu" id="view_menu" width="160px">
    <tr><td class="menu" onclick="colorMenuClick();">Color Scheme</td></tr>
    <tr><td class="menu" onclick="highlighMenuClick();">Highlight Language</td></tr>
    </table>
-
 </td>
 <td class="menu_item" onclick="addBookmarkMenuClick();">+Bookmark</td>
 <td class="menu_item" onclick="findBookmarkMenuClick();">Find Bookmark</td>
-<td>CCP</td>
+<td class="menu_icon"><img src="images/cut.bmp" alt="Cut" onClick="cutIconClicked();"/></td>
+<td class="menu_icon"><img src="images/copy.bmp" alt="Copy" onClick="copyIconClicked();"/></td>
+<td class="menu_icon"><img src="images/paste.bmp" alt="Paste" onClick="pasteIconClicked();"/></td>
 </tr>
 </table>
--- trunk/messenger.php
+++ trunk/messenger.php
@@ -15,14 +15,14 @@ Contacts
 <td>
 <div style="overflow:auto;background-color:white;margin-left:5px;margin-right:5px;height:250px;">
 <ul class="contacts">
-<li><div class="contact_group" onClick="showHideMessengerGroup('online');">Online</div>
+<li><div class="contact_group" onClick="showHideMessengerGroup('online');changeMessengerArrow('online_arrow');"><img id="online_arrow" src="images/arrow-down.gif">&nbsp;Online</div>
 <ul id="online" class="contacts">
 <li class="contact_new_msg" onClick="openPopup('user1','Another User 1','chat');">Another User 1</li>
 <li class="contact_old_msg" onClick="openPopup('user2','Another User 2','chat');">Another User 2</li>
 <li class="contact" onClick="openPopup('user3','Another User 3','chat');">Another User 3</li>
 </li>
 </ul>
-<li><div class="contact_group" onClick="showHideMessengerGroup('offline');">Offline</div>
+<li><div class="contact_group" onClick="showHideMessengerGroup('offline');changeMessengerArrow('offline_arrow');"><img id="offline_arrow" src="images/arrow.gif">&nbsp;Offline</div>
 <ul id="offline" class="contacts" style="display: none;">
 <li class="contact_new_msg">another user</li>
 <li class="contact_old_msg">another user</li>
@@ -34,6 +34,13 @@ Contacts
 </ul>
 </li>
 </ul>
+</div>
+</td>
+</tr>
+<tr>
+<td>
+<div class="messenger_button" onClick="openPopup('addContact','Add a Contact','addContact');">
++Contact
 </div>
 </td>
 </tr>
