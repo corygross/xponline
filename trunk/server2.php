0ed9ef2bb6ed5cd225e9f59b80d1147ab048d997
corygross <corygross@0a18d55e-ba55-0410-9722-499b6f56dc25>
2008-10-12 23:56:42 +0000

    chat!

--- trunk/server2.php
+++ trunk/server2.php
@@ -1,9 +1,17 @@
 <?php
-$myId = "001";
+session_start();
+require_once "dbConnect.php";
 
-static $messageId = 0;
-$messageId++;
+$currentUserID = $_SESSION['uID'];
 
+//UPDATE when the last connection was made
+$sqlUpdateTime = "UPDATE users SET uLastActivity=CURRENT_TIMESTAMP WHERE uID='$currentUserID';";
+runQuery($sqlUpdateTime);
+
+//$myId = "001";
+
+//static $messageId = 0;
+//$messageId++;
 if (isset($_GET["input"]))
 {
 	$userInput = $_GET["input"];
@@ -19,11 +27,11 @@ $tokenArray =  array();
 
 
 //Connect to database
-$link = mysql_connect('localhost', 'root', 'root') or die('could not connect'.mysql_error());
-$output_query = "SELECT * FROM msgqueue WHERE mID = $myId";
+//$link = mysql_connect('localhost', 'root', 'spider') or die('could not connect'.mysql_error());
+//$output_query = "SELECT * FROM msgqueue WHERE mID = $myId";
 
 //Select our database
-mysql_select_db("xponline", $link);
+//mysql_select_db("xponline", $link);
 
 if($userInput != '')
 {
@@ -43,23 +51,31 @@ if (ob_get_level() == 0) ob_start();
  *
  *
  */
-function getMessage()
+function getMessage($currentUserID)
 {
-	$resultSet = mysql_query($GLOBALS['output_query']);
+	$output_query = "SELECT * FROM msgqueue,users WHERE msgqueue.toID = '$currentUserID' AND msgqueue.fromID=users.uID ORDER BY sentTime ASC;";
+	//echo $output_query;
+	$resultSet = runQuery($output_query);
 
+	//$resultSet = mysql_query($GLOBALS['output_query']);
 	while ($row = mysql_fetch_array($resultSet))
-	{
-		echo $row['msg'];
-		echo "</br>";
-	 	echo str_pad('',4096)."\n";  //Safari need xx characters to first recognize pushed content.
-		echo "</body>"; //This is key to have this here, at least for Safari
+	{	
+		echo $row['fromID']."&^*".$row['uFName']." ".$row['uLName']."&^*".$row['msg']."&^*";
+		$mID = $row['mID'];
+		$delSQL = "DELETE from msgqueue WHERE mID='$mID';";
+		runQuery($delSQL);
+		
+	 	//echo str_pad('',4096)."\n";  //Safari need xx characters to first recognize pushed content.
+		//echo "</body>"; //This is key to have this here, at least for Safari
 		ob_flush();
 	    flush();
 		//Wait a second
-	   	usleep(1000000);
-
+	   	//usleep(10000000);
+		
 	}
-	
+	//ob_flush();
+	//flush();
+	usleep(100000);
 }
 
 /*
@@ -75,12 +91,18 @@ function insertMessage($inArray)
 
 	static $msgId = 0;
 	$msgId++;
+
+
 	//Constrct the query string from user parameters and execute
 	//$insertQuery = "INSERT into msgqueue (mId, fromID, toID, msg, sentTime) VALUES ('3', '2', '1', 'Test', '2008-10-07 19:44:2')" or die("error with insert");
 //	$sql = 'INSERT INTO `xponline`.`msgqueue` (`mID`, `fromID`, `toID`, `msg`, `sentTime`) VALUES ('$input1', 2, 1, "ron is testing again 3", CURRENT_TIMESTAMP);';
-	$sql = "INSERT INTO msgqueue VALUES('$msgId', '$input1', '$input2', '$input3', CURRENT_TIMESTAMP)";
-	$result = mysql_query($sql);
-	echo mysql_errno(). ": " . mysql_error();
+	//$sql = "INSERT INTO msgqueue VALUES('$msgId', '$input1', '$input2', '$input3', CURRENT_TIMESTAMP)";
+	
+	//setting the message ID as null allows it to auto-increment
+	$sql = "INSERT INTO msgqueue VALUES(null, '$input1', '$input2', '$input3', CURRENT_TIMESTAMP)";
+	//$result = mysql_query($sql);
+	$result = runQuery($sql);
+	//echo mysql_errno(). ": " . mysql_error();
 }
 
 /*
@@ -99,13 +121,11 @@ function parseInput($uInput)
 	
 }
 
-
-
 //Check for messages, and if found, send to client
-for ($i = 0; $i<10; $i++) { getMessage(); }
+for ($i = 0; $i<10; $i++) { getMessage($currentUserID); }
 
 ob_end_flush(); //necessary
-//echo $_GET['input'];
-mysql_close($link); //Close the connection when done
+
+//mysql_close($link); //Close the connection when done
 
 ?>
