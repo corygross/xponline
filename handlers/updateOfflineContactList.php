6e5aa78a934b9bc843409db64dde83d6c41f6731
corygross <corygross@0a18d55e-ba55-0410-9722-499b6f56dc25>
2008-10-14 23:09:11 +0000

    contact list is now alphabetically sorted

--- handlers/updateOfflineContactList.php
+++ handlers/updateOfflineContactList.php
@@ -9,7 +9,7 @@ if($currentUserID == ""){
 	return;
 }
 
-$sqlGetOnlineContacts = "SELECT * FROM contacts,users WHERE ((uID1='$currentUserID' AND uID2=uID ) OR (uID2='$currentUserID' AND uID1=uID)) AND (u1accept='1' AND u2accept='1') AND (uLastActivity<DATE_SUB(CURRENT_TIMESTAMP,INTERVAL 30 SECOND));";
+$sqlGetOnlineContacts = "SELECT * FROM contacts,users WHERE ((uID1='$currentUserID' AND uID2=uID ) OR (uID2='$currentUserID' AND uID1=uID)) AND (u1accept='1' AND u2accept='1') AND (uLastActivity<DATE_SUB(CURRENT_TIMESTAMP,INTERVAL 30 SECOND)) ORDER BY users.uFName ASC, users.uLName ASC;";
 $onlineResults = runQuery($sqlGetOnlineContacts);
 
 while ($row = mysql_fetch_array($onlineResults))
