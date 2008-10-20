ff2c6a9d03e4252186372efc23ee8749153c8a82
corygross <corygross@0a18d55e-ba55-0410-9722-499b6f56dc25>
2008-10-20 04:06:07 +0000

    reorganization

--- bm.js
+++ bm.js
@@ -1,12 +1,19 @@
-var selectedBM = null;
-var gotoLine = -1;
-function selectBM(obj, line){
-	if(selectedBM != null){
-		selectedBM.className = "bm";
+function addBM(windowID){
+	var text = document.getElementById('bmText').value;
+	var newBM = get_bm_value() + ": " + text;
+	alert(newBM);
+	hidePopup(windowID);
+}
+
+function get_bm_value()
+{
+	for (var i=0; i < document.addBMForm.bmType.length; i++)
+	{
+	if (document.addBMForm.bmType[i].checked)
+		{
+			return document.addBMForm.bmType[i].value;
+		}
 	}
-	obj.className = "bm_selected";	
-	selectedBM = obj;
-	gotoLine = line;
 }
 
 function goToBM(windowID){
@@ -19,20 +26,13 @@ function goToBM(windowID){
 	}
 }
 
-function addBM(windowID){
-	var text = document.getElementById('bmText').value;
-	var newBM = get_bm_value() + ": " + text;
-	alert(newBM);
-	hidePopup(windowID);
-}
-
-function get_bm_value()
-{
-for (var i=0; i < document.addBMForm.bmType.length; i++)
-   {
-   if (document.addBMForm.bmType[i].checked)
-      {
-	      return document.addBMForm.bmType[i].value;
-      }
-   }
+var selectedBM = null;
+var gotoLine = -1;
+function selectBM(obj, line){
+	if(selectedBM != null){
+		selectedBM.className = "bm";
+	}
+	obj.className = "bm_selected";	
+	selectedBM = obj;
+	gotoLine = line;
 }
