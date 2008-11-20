b03ea80364827b54993942bf24e7f81eba466d7d
corygross <corygross@0a18d55e-ba55-0410-9722-499b6f56dc25>
2008-11-20 20:09:07 +0000

    fixed problematic slashes

--- handlers/updatePendingContactsButton.php
+++ handlers/updatePendingContactsButton.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 
-require_once $_SESSION['homeDir']."\dbConnect.php";
+require_once $_SESSION['homeDir']."/dbConnect.php";
 
 function getButton(){
 	if(isset($_SESSION['uID'])) { $uID = $_SESSION['uID']; }
