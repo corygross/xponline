07c0d6f7f95e90397e79e2a34d9bc3d473121bae
corygross <corygross@0a18d55e-ba55-0410-9722-499b6f56dc25>
2008-11-26 06:20:43 +0000

    multi-language syntax highlighting

--- syntaxHighlight/highlight_words.js
+++ syntaxHighlight/highlight_words.js
@@ -4,8 +4,8 @@
  *   Each word should also have a matching reference in a stylesheet named for the language
  */
 
- // Keywords for java
-var java_words = "~public~system~abstract~continue~for~while~new~switch~assert~default~goto~package~synchronized~boolean~do~if~private~this~break~double~implements~protected~throw~byte~else~import~public~throws~case~enum~instanceof~return~transient~catch~extends~int~short~try~char~final~interface~static~void~class~finally~long~strictfp~volatile~const~int~String~";
+// Keywords for java
+var java_words = "~public~system~abstract~continue~for~while~new~switch~assert~default~goto~package~synchronized~boolean~do~if~private~this~break~double~implements~protected~throw~byte~else~import~public~throws~case~enum~instanceof~return~transient~catch~extends~int~short~try~char~final~interface~static~void~class~finally~long~strictfp~volatile~const~int~String~true~false~null~";
 
 // Default keywords.  These will be used if we don't recognize a file type
-var default_words = "~for~while~do~if~else~boolean~bool~int~double~break~return~";
+var default_words = "~for~while~do~if~else~boolean~bool~int~double~break~return~true~false~null~";
